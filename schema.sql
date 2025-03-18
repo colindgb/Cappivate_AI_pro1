a830b80c4917ad0e8af6002ccb54c413e9b429f1
colindgb <43166726+colindgb@users.noreply.github.com>
2025-03-18 20:07:16 +1000

    Delete schema.sql

--- schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE cappivate_ai;
-CREATE TABLE linked_accounts (
-    id SERIAL PRIMARY KEY,
-    user_id VARCHAR(50) NOT NULL,
-    platform VARCHAR(20) NOT NULL,
-    profile_url TEXT NOT NULL
-);
